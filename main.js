1462ae1132d4bb196578661d327a48adff9de14d
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-11-30 15:37:41 +1300

    updated style

--- main.js
+++ main.js
@@ -707,4 +707,25 @@ function open_book() {
   book.style.transform = "translateX(0%)";
 }
 
+function PrintDiv(id) {
+  var data=document.getElementById(id).innerHTML;
+  var myWindow = window.open('', 'my div', 'height=400,width=600');
+  myWindow.document.write('<html><head><title>my div</title>');
+  /*optional stylesheet*/ //myWindow.document.write('<link rel="stylesheet" href="main.css" type="text/css" />');
+  myWindow.document.write('</head><body >');
+  myWindow.document.write(data);
+  myWindow.document.write('</body></html>');
+  myWindow.document.close(); // necessary for IE >= 10
+
+  myWindow.onload=function(){ // necessary if the div contain images
+
+      myWindow.focus(); // necessary for IE >= 10
+      myWindow.print();
+      myWindow.close();
+  };
+}
+
+window.PrintDiv = PrintDiv;
+
+
 window.open_book = open_book;
